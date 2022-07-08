7687df2d6c1a8d764b0ffce8d5b63e815de27aaa
Asghar Ali Mowlana <alimowlana24@gmail.com>
2022-07-08 13:59:13 +0530

    Modified service page

--- components/ServiceCard.js
+++ components/ServiceCard.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {StyleSheet, View, Text, TextInput, StatusBar} from 'react-native';
+import {StyleSheet, View, Text, Image} from 'react-native';
 
 const ServiceCard = ({
   serviceTitle,
@@ -9,7 +9,14 @@ const ServiceCard = ({
 }) => {
   return (
     <View style={styles.container}>
-      <View style={styles.imageContainer}></View>
+      <View style={styles.imageContainer}>
+        <Image
+          style={styles.image}
+          source={{
+            uri: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQKuySdKYOAP7D4P_OUAy2YvVIpobnm9HJWvMmSJ9aZAw&s',
+          }}
+        />
+      </View>
       <View style={styles.detailsContainer}>
         <Text style={styles.textStyle}>{serviceTitle}</Text>
         <Text style={styles.textStyle}>{serviceAmount}</Text>
@@ -23,6 +30,11 @@ const ServiceCard = ({
 const styles = StyleSheet.create({
   container: {},
   imageContainer: {},
+  image: {
+    width: '70%',
+    height: '50%',
+    borderRadius: 100,
+  },
   detailsContainer: {},
   textStyle: {
     color: 'black',
